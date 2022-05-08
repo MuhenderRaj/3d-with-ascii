53da0240404157691e8fd8024572f402bf403329
muhender <rkmr02@gmail.com>
2022-05-08 00:02:19 -0400

    Add spec and tests for Quaternion operations

--- Typescript/src/math/quaternion.ts
+++ Typescript/src/math/quaternion.ts
@@ -6,6 +6,8 @@ import { Vector3 } from './vector';
 export class Quaternion {
     public static readonly identity: Quaternion = new Quaternion(1, Vector3.zero);
 
+    /// CREATORS ///
+
     /**
      * Construct a new Quaternion object
      * 
@@ -45,5 +47,88 @@ export class Quaternion {
         return new Quaternion(realPart, vectorPart);
     }
 
+    /// QUATERNION SCALAR OPERATIONS ///
 
+    /**
+     * 
+     * @param num the number to be multiplied to the quaternion
+     * @returns a new quaternion with its components scaled by `number`
+     */
+    public multiplyByScalar(num: number): Quaternion { }
+
+    /**
+     * 
+     * @param num the number to divide the quaternion by
+     * @returns a new quaternion with its components divided by `number`
+     * @throws Error if num is zero
+     */
+    public divideByScalar(num: number): Quaternion { }
+    
+    /// QUATERNION LINEAR OPERATIONS ///
+
+    /**
+     * 
+     * @param other the other quaternion to add
+     * @returns the sum of the quaternions `this` and `other` as a new quaternion
+     */
+    public add(other: Quaternion): Quaternion { }
+    
+    /**
+     * 
+     * @param other the other quaternion to subtract from `this`
+     * @returns the difference of the quaternions `this` and `other` as a new quaternion
+     */
+    public subtract(other: Quaternion): Quaternion { }
+
+    /**
+     * @returns the negation of the components of `this` as a new quaternion
+     */
+    public negate(): Quaternion { }
+    
+    /// QUATERNION MULTIPLICATION AND DIVISION ///
+
+    /**
+     * 
+     * @param other the other quaternion to compute the quaternion product
+     * @returns the quaternion product `this`*`other`
+     */
+    public multiply(other: Quaternion): Quaternion { }
+    
+    /**
+     * 
+     * @param other the other quaternion to divide the quaternion by
+     * @returns the quaternion division result of `this`/`other`
+     * @throws Error if `other` is the zero quaternion
+     */
+    public divideBy(other: Quaternion): Quaternion { }
+    
+    /**
+     * @returns a new quaternion that is the inverse of `this`, 
+     *      such that this*this.inverse() = 1
+     * @throws Error if `this` is the zero quaternion
+     */
+    public inverse(): Quaternion { }
+
+    /// QUATERNION REPRESENTATION ///
+
+    /**
+     * @inheritdoc
+     */
+    public toString(): string { }
+    
+    /**
+     * @returns an array of the components of the quaternion, in order real, i, j, k
+     */
+    public asArray(): [number, number, number, number] { }
+    
+    /**
+     * @returns a record type of the axis and angle of rotation encoded by this quaternion
+     */
+    public asAxisAngle(): { axis: number, angle: number } { }
+    
+    /**
+     * @returns the Euler angles associated with this quaternion as a record type
+     */
+    public asEuler(): { rotX: number, rotY: number, rotZ: number } { }
+    
 }
--- Typescript/test/quaternionTest.ts
+++ Typescript/test/quaternionTest.ts
@@ -20,4 +20,49 @@ describe('Quaternion tests', function () {
 
         assert(checkQuaternionEquality(q, new Quaternion(0, new Vector3(1, 0, 0))), "Wrong quaternion");
     });
+
+    it('fromEuler tests', function () {
+        const q_x = Quaternion.fromEuler(Math.PI, 0, 0);
+        const q_y = Quaternion.fromEuler(0, Math.PI, 0);
+        const q_z = Quaternion.fromEuler(0, 0, Math.PI);
+        const q = Quaternion.fromEuler(Math.PI, Math.PI, Math.PI);
+
+        assert(checkQuaternionEquality(q_x, new Quaternion(0, Vector3.left)), "Wrong quaternion");
+        assert(checkQuaternionEquality(q_y, new Quaternion(0, Vector3.up)), "Wrong quaternion");
+        assert(checkQuaternionEquality(q_z, new Quaternion(0, Vector3.forward)), "Wrong quaternion");
+        assert(checkQuaternionEquality(q, new Quaternion(1, Vector3.zero)), "Wrong quaternion");
+    });
+
+    it('multiply and divide tests', function () {
+        const q1 = new Quaternion(2, new Vector3(0, 0, 0));
+        const q2 = new Quaternion(0, new Vector3(0, 2, 3));
+
+        assert(checkQuaternionEquality(q1.multiply(q2), new Quaternion(0, new Vector3(0, 4, 6))), "Incorrect result of multiplication");
+        assert(checkQuaternionEquality(q2.multiplyByScalar(3), new Quaternion(0, new Vector3(0, 6, 9))), "Incorrect result of multiplication by scalar");
+        assert(checkQuaternionEquality(q1.multiply(new Quaternion(0, Vector3.zero)) , new Quaternion(0, Vector3.zero)), "Incorrect result of multiplying by 0");
+
+        assert(checkQuaternionEquality(q2.divideByScalar(2), new Quaternion(0, new Vector3(0, 1, 1.5))), "Incorrect result of dividing by scalar");
+        assert(checkQuaternionEquality(q2.divideBy(new Quaternion(0, new Vector3(1, 0, 0))), new Quaternion(0, new Vector3(0, -3, 2))), "Incorrect result of dividing by quaternion");
+
+        assert.throws(function () {
+            q1.divideByScalar(0);
+        }, "Dividing by zero should throw error");
+
+        assert.throws(function () {
+            q1.divideBy(new Quaternion(0, Vector3.zero));
+        }, "Dividing by zero should throw error");
+    });
+
+    it('add, subtract, and negate tests', function () {
+        const q1 = new Quaternion(4, new Vector3(9, 5, 3));
+        const q2 = new Quaternion(4, new Vector3(8, 1, -1));
+
+        assert(checkQuaternionEquality(q1.add(q2), new Quaternion(8, new Vector3(17, 6, 2))), "Incorrect sum of quaternions");
+        assert(checkQuaternionEquality(q1.add(new Quaternion(0, Vector3.zero)), q1), "Incorrect sum with zero quaternion");
+
+        assert(checkQuaternionEquality(q1.subtract(q2), new Quaternion(0, new Vector3(1, 4, 4))), "Incorrect difference of quaternions");
+        assert(checkQuaternionEquality(q1.subtract(new Quaternion(0, Vector3.zero)), q1), "Incorrect difference with zero quaternion");
+
+        assert(checkQuaternionEquality(q1.negate(), new Quaternion(0, new Vector3(-9, -5, -3))), "Incorrect negation of quaternion");
+    });
 });
