86d98be7915843402c2f32af8bfa67b2684dfbb5
muhender <rkmr02@gmail.com>
2022-05-17 19:19:20 -0400

    Add AsciiCamera and index.html

---
+++ Typescript/src/builtins/cameras/AsciiCamera.ts
@@ -0,0 +1,32 @@
+import { Canvas } from "canvas";
+import { Environment } from "../../environment";
+import { Quaternion } from "../../math/quaternion";
+import { Vector3 } from "../../math/vector";
+import { Camera } from "../interfaces";
+
+export class AsciiCamera implements Camera {
+    private readonly zBuffer: Array<Array<number>>;
+
+    public constructor(
+        public position: Vector3,
+        public rotation: Quaternion,
+        public scaling: Vector3,
+        public hidden: boolean,
+        public width: number,
+        public height: number,
+        public zoom: number,
+        public perspective: boolean,
+        public depth: number) {
+    
+        this.zBuffer = new Array<number>(height)
+            .map(el => new Array<number>(width).fill(Number.POSITIVE_INFINITY));
+    }
+
+
+
+    public render(env: Environment): Canvas {
+        throw new Error("Method not implemented.");
+    }
+
+    
+}
